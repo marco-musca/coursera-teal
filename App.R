8e1fc0e1ee794888a268956e5b627777bc447f7b
Marco <marco.musca96@gmail.com>
2026-05-19 21:10:57 +0200

    Adding filters

--- App.R
+++ App.R
@@ -4,8 +4,10 @@ app <- init(
   data = teal_data(IRIS = iris, MTCARS = mtcars),
   modules = modules(
     example_module(label = "Example module")
-  )
-) |>
+  ),
+  filter = teal_slices(
+    teal_slice(dataname = "IRIS", varname = "Species", selected = "setosa", fixed = TRUE),
+    teal_slice(dataname ="IRIS", varname = "Sepal.Length", anchored = TRUE)))|>
   modify_title("My first teal app") |>
   modify_header("My first teal app") |>
   modify_footer("This is a test")
